8470d62fd44132a08529417a70bdd75e08e62c2c
Edmund Eli Bansah <edmundbansah@gmail.com>
2023-12-07 18:43:14 +0000

    Adding path to python script and correcting pycodestyle errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 import os
 from fabric.api import task, local
 from datetime import datetime
+
 
 @task
 def do_pack():
@@ -11,7 +14,8 @@ def do_pack():
     # Create the versions folder if it doesn't exist
     local('mkdir -p versions')
 
-    # Generate the archive filename (web_static_<year><month><day><hour><minute><second>.tgz)
+    # Generate the archive filename
+    # (web_static_<year><month><day><hour><minute><second>.tgz)
     timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     archive_name = f'web_static_{timestamp}.tgz'
     archive_path = os.path.join('versions', archive_name)
